d5d771efe437b0272eecbf915d0938dd0e4b2654
Riya-JAIN2001 <167556137+Riya-JAIN2001@users.noreply.github.com>
2024-04-20 22:57:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-
-import './App.css'
-
-import Dashboard from './pages/Dashboard/Dashboard'
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
 import Support from './pages/Support/Support';
 import Transaction from './pages/Transaction/Transaction';
 
-
-const router= createBrowserRouter([
-  { 
-    path:"/",
-    element: <Dashboard/>
-  },
-  { 
-    path:"/transaction",
-    element: <Transaction/>
-  },
-  { 
-    path:"/support",
-    element: <Support/>
-  },
-
-])
 function App() {
-  
   return (
-    <>
-    <RouterProvider router={router}/>
-    
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
